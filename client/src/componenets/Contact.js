2fdfa7d5214f02bcd1043b4c081eb9e79a94fa79
madrodgerflynn <madrodgerflynn@gmail.com>
2022-10-10 18:16:02 -0600

    added a modal in Contact.js

--- client/src/componenets/Contact.js
+++ client/src/componenets/Contact.js
@@ -1,1 +1,25 @@
-import react from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+function Contact() {
+  return (
+    <Modal.Dialog>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal title</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <p>
+          GitHub:
+          <link href="https://github.com/madrodgerflynn">@madrodgerflynn</link>
+        </p>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="primary">Close</Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  );
+}
+
+export default Contact;
