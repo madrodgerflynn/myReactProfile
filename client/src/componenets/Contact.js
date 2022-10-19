e8793c24c0a7d9d25ee65acf33b3d5b292715e6c
madrodgerflynn <madrodgerflynn@gmail.com>
2022-10-19 15:31:11 -0600

    initial commit

--- client/src/componenets/Contact.js
+++ client/src/componenets/Contact.js
@@ -41,3 +41,5 @@ function Contact() {
 }
 
 render(<Contact />);
+
+export default Contact;
--- client/src/componenets/MyShtuff.js
+++ client/src/componenets/MyShtuff.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
 
-export default function MyShtuff() {
+function MyShtuff() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    //     <div className="row">
-    //         <h1>My Bio</h1>
-    //         <div className="row">
-    //             <p>
-    //                Welcome! My name is Tyler Anderson and this is a little about me.
-    //             </p>
-    //             <p>
-    //                 I have always had an interest in all things computer related, but got distracted by another major when I was in college. I was given an opportunity to go back to school for an Associates in Electrical Engineering
-    //             </p>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
 
-    //         </div>
-    //     </div>
-    <div>Hello World!</div>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
+
+render(<MyShtuff />);
